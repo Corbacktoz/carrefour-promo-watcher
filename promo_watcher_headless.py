1b2e06e305d922092958f1e60b8a1d0cf2662ecd
Corbacktoz <corback.inc@gmail.com>
2025-10-05 16:35:48 +0200

    Ajout Carrefour, Chronodrive et Amazon aux cibles

--- promo_watcher_headless.py
+++ promo_watcher_headless.py
@@ -14,8 +14,12 @@ from playwright.async_api import async_playwright, TimeoutError as PWTimeout
 
 # === Configuration ===
 PROMO_URLS = [
-    "https://www.auchan.fr/catalogue/",
+"https://www.auchan.fr/catalogue/",
     "https://www.e.leclerc/prospectus",
+    "https://www.intermarche.com/catalogue",
+    "https://courses.carrefour.fr/promotions",
+    "https://www.chronodrive.com/fr/promotions",
+    "https://www.amazon.fr/gp/goldbox",
 ]
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
